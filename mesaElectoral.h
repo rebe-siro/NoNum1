f55ca10e9f8089e9037350b2be75f7830ab81592
MarySPerez <108902295+MarySPerez@users.noreply.github.com>
2024-10-28 18:17:40 -0400

    Update mesaElectoral.h

--- mesaElectoral.h
+++ mesaElectoral.h
@@ -1,1 +1,27 @@
+#ifndef MESAELECTORAL_H
+#define MESAELECTORAL_H
 
+#include <vector>
+#include <string>
+#include "candidato.h"
+
+using namespace std;
+
+class Mesa {
+
+   private:
+   	std::vector<Candidato> candidatos;
+
+   public:
+    // Constructor
+    MesaElectoral() = default;
+
+    // MÃ©todo para registrar un candidato
+    void registrarCandidato(const Candidato& candidato);
+
+    // MÃ©todo para mostrar todos los candidatos
+    void mostrarCandidatos() const;
+      
+};
+
+#endif // MESA_ELECTORAL_H_

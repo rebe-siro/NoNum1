81bf660bc0a99b27a2ed9134cd947556cddf81a9
MarySPerez <108902295+MarySPerez@users.noreply.github.com>
2024-10-28 18:17:16 -0400

    Update candidato.h

--- candidato.h
+++ candidato.h
@@ -1,1 +1,30 @@
+#ifndef CANDIDATO_H_
+#define CANDIDATO_H_
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//Clse para representar a un candidato
+
+class Candidato {
+	private:
+		strign nombre;
+		string partido;
+		
+	public:
+		Candidato();
+		void setNombre (string n);
+		string getNombre ();
+		void setPartido (string prd);
+		string getPartido ();
+		
+		void mostrarInformacion() const {
+		cout << "Nombre: " << nombre << endl;
+        cout << "Partido: " << partido << endl;
+		}
+};
+
+#endif /* CANDIDATO_H_ */
 

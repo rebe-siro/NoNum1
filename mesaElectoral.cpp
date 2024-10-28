b4c50829436cc0040965fa50104d5c59ef73667c
MarySPerez <108902295+MarySPerez@users.noreply.github.com>
2024-10-28 18:17:55 -0400

    Update mesaElectoral.cpp

--- mesaElectoral.cpp
+++ mesaElectoral.cpp
@@ -1,1 +1,20 @@
+#include "MesaElectoral.h"
 
+using namespace std;
+
+void MesaElectoral::registrarCandidato(const Candidato& candidato) {
+    candidatos.push_back(candidato);
+    cout << "Candidato " << candidato.getNombre() << " registrado exitosamente." << endl;
+}
+
+void MesaElectoral::mostrarCandidatos() const {
+    if (candidatos.empty()) {
+        cout << "No hay candidatos registrados." << endl;
+    } else {
+        cout << "Lista de candidatos registrados:" << endl;
+        for (const auto& candidato : candidatos) {
+            candidato.mostrarInformacion();
+            cout << "-------------------------" << endl;
+        }
+    }
+}
